de6b69577a5c2a8d7622e6a57e5336905de09688
Arthur Hermes <89710458+ArthurHermes@users.noreply.github.com>
2024-10-18 08:52:26 -0300

    a

--- Zoologico/RegexEmailValidator.java
+++ Zoologico/RegexEmailValidator.java
@@ -1,7 +1,6 @@
 import java.util.regex.Pattern;
 
 public class RegexEmailValidator implements ValidadorEmailAdapter {
-    // Expressão regular para validar emails
     private static final String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
     private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
 
